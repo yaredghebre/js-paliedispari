2f75d06649686fe057abd86d3c6d573ca8fc43a2
yaredghebre <yared2391@gmail.com>
2023-03-16 18:18:28 -0300

    Add: Pari Dispari (upd)

--- Pari-Dispari/js/script.js
+++ Pari-Dispari/js/script.js
@@ -4,13 +4,13 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-
+// Chiedo informazioni all'utente
 const userChoice = prompt("PARI o DISPARI?")
 const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-// const userResult = oddOrEven(userNumber);
 console.log(userChoice, userNumber);
 alert("Hai lanciato un " + userNumber);
 
+// Creo variabile per lancio numero al computer
 const computerNumber = randomNumber();
 alert("Ho lanciato un " + computerNumber);
 
@@ -18,12 +18,13 @@ console.log(computerNumber);
 
 gameResult(userNumber, computerNumber, userChoice);
 
+// funzione per fare la sommma dei risultati e condizioni per stabilire il vincitore
 function gameResult(userNumber, computerNumber, userChoice) {
     let sum = userNumber + computerNumber;
 
     console.log(sum);
 
-    let result;
+
     if (sum % 2 === 0) {
         console.log("even");
         if (userChoice === "PARI") {
@@ -42,7 +43,7 @@ function gameResult(userNumber, computerNumber, userChoice) {
     }
   
 }
-
+// creo funzione del per numero casuale computer
 function randomNumber() {
     const rndNumber = Math.floor(Math.random() * 5) + 1;
     return rndNumber;
